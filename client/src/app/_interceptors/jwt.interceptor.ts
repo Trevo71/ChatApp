091bd38e2327f36f2070876c13b60c72e4429c87
Trevin Carson <trevincarson96@yahoo.com>
2021-02-08 11:03:00 -0600

    added form logic and styling to edit profile form

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,41 @@
+/**
+ * Jwt Interceptor to intercept current user of application and set the JWT of the session on each session.
+ */
+
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { take } from 'rxjs/operators';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+  constructor(private accountService: AccountService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    let currentUser: User;
+
+    this.accountService.currentUser$
+      //take pipe operator to make sure to unsubscribe from observables(technique of completing subscriptions)
+      .pipe(take(1))
+      .subscribe((user) => (currentUser = user));
+    //If logged in attatch the token to every request to maintain valid session for the current user
+    if (currentUser) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      });
+    }
+    return next.handle(request);
+  }
+}
--- client/src/app/_modules/routing.module.ts
+++ client/src/app/_modules/routing.module.ts
@@ -10,6 +10,7 @@ import { AuthGuard } from '../_guards/auth.guard';
 import { TestErrorsComponent } from '../errors/test-errors/test-errors.component';
 import { NotFoundComponent } from '../errors/not-found/not-found.component';
 import { ServerErrorComponent } from '../errors/server-error/server-error.component';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,9 +22,9 @@ const routes: Routes = [
       {
         path: 'members',
         component: MemberListComponent,
-        canActivate: [AuthGuard],
       },
-      { path: 'members/:id', component: MemberDetailComponent },
+      { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'member/edit', component: MemberEditComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ],
