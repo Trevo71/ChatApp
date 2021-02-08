e594edb5962ef727a7836c6b6ad2be430190f5cf
Trevin Carson <trevincarson96@yahoo.com>
2021-02-08 14:43:09 -0600

    added API call functionality on the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -4,7 +4,7 @@ import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
@@ -17,5 +17,9 @@ export class MembersService {
 
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member);
   }
 }
