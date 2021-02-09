ab5ec97b9b6dfbca814a9447ea90d62d009fa7dc
Trevin Carson <trevincarson96@yahoo.com>
2021-02-09 10:33:36 -0600

    updated user data to only make the api call to the database when the user's information is not available on the application

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -38,20 +40,14 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     NotFoundComponent,
     ServerErrorComponent,
     MemberCardComponent,
-    MemberEditComponent,
+    MemberEditComponent
   ],
-  imports: [
-    BrowserModule,
-    RoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    SharedModule,
-  ],
+  imports: [BrowserModule, RoutingModule, HttpClientModule, BrowserAnimationsModule, FormsModule, SharedModule, NgxSpinnerModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
