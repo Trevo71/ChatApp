ad0e6b298b737283bead80633347a12760ab2e83
Trevin Carson <trevincarson96@yahoo.com>
2021-02-17 16:41:55 -0600

    added functionality on the client to add the picture of current user to the navigation bar/ added functionality to delete photos from database and client

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -6,7 +6,7 @@ import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
@@ -22,14 +22,14 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.next(user);
   }
 
@@ -42,8 +42,7 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.next(user);
+          this.setCurrentUser(user);
         }
       })
     );
