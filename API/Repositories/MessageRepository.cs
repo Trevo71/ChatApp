ffc4502c5e85c1a11cf09e7a6398f9ed0effec86
Trevin Carson <trevincarson96@yahoo.com>
2021-03-15 07:48:08 -0500

    added the delete functionality to the client

--- API/Repositories/MessageRepository.cs
+++ API/Repositories/MessageRepository.cs
@@ -36,7 +36,10 @@ namespace API.Repositories
 
         public async Task<Message> GetMessage(int id)
         {
-            return await _context.Messages.FindAsync(id);
+            return await _context.Messages
+            .Include(u => u.Sender)
+            .Include(u => u.Recipient)
+            .SingleOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -46,9 +49,9 @@ namespace API.Repositories
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username),
-                _ => query.Where(u => u.Recipient.UserName == messageParams.Username && u.DateRead == null)
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username && u.SenderDeleted == false),
+                _ => query.Where(u => u.Recipient.UserName == messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -62,10 +65,10 @@ namespace API.Repositories
             var messages = await _context.Messages
                             .Include(u => u.Sender).ThenInclude(p => p.Photos)
                             .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                            .Where(m => m.Recipient.UserName == currentUsername
+                            .Where(m => m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
                             
                                  && m.Sender.UserName == recipientUsername || m.Recipient.UserName == recipientUsername
-                                 && m.Sender.UserName == currentUsername
+                                 && m.Sender.UserName == currentUsername && m.SenderDeleted == false
                             
                             
                             )
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -35,4 +35,13 @@ export class MessagesComponent implements OnInit {
 
     this.loadMessages();
   }
+
+  deleteMessage(id: number) {
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(
+        this.messages.findIndex((m) => m.id === id),
+        1
+      );
+    });
+  }
 }
